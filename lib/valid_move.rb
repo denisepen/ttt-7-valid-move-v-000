b916baddf20bfe0b6a5012262ae513139f71ba4f
Denise Pendleton <dpendl1@hotmail.com>
2018-02-13 16:39:08 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -17,7 +17,7 @@ if index.between?(0, 8)  #|| position_taken?(board, index)
   return "true"
 end
 
-if board[index] != "" || board[index] != " "
+if board[index] == "X" && board[index] == "O"
  return "false"
 end
 
