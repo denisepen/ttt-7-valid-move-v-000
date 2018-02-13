31b7bb06f33875df7285296784ef1f27e51fa71e
Denise Pendleton <dpendl1@hotmail.com>
2018-02-13 16:42:20 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -16,7 +16,7 @@ def valid_move? (board, index)
   if board[index] == "X" || board[index] == "O"
    return "false"
   end
-    
+
 if index.between?(0, 8)  #|| position_taken?(board, index)
   return "true"
 end
