bf923d69b7b7a0161ce0714d28ce1ee87d32425a
Denise Pendleton <dpendl1@hotmail.com>
2018-02-13 16:31:42 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,20 +1,20 @@
 
 def valid_move? (board, index)
-  def position_taken?(board, index)
-   if board[index] == "X" || board[index] == "O"
-    return true
-  end
-
-  if board[index] == " " || board[index] == ""
-    return false
-  end
-
-  if board[index] = "nil"
-    return false
-  end
-  end
-if index.between?(0, 8) || position_taken? (board, index)
-  return "false"
+  # def position_taken?(board, index)
+  #  if board[index] == "X" || board[index] == "O"
+  #   return true
+  # end
+  # 
+  # if board[index] == " " || board[index] == ""
+  #   return false
+  # end
+  # 
+  # if board[index] = "nil"
+  #   return false
+  # end
+  # end
+if index.between?(0, 8)  #|| position_taken?(board, index)
+  return "true"
 end
 
 
