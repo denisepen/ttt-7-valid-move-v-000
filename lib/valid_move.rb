fb40ca29828c62a276a20e94897122cb9c783909
Denise Pendleton <dpendl1@hotmail.com>
2018-02-13 16:27:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,39 @@
+
+
+
 # code your #valid_move? method here
 
+def position_taken?(board, index)
+ if board[index] == "X" || board[index] == "O"
+  return true
+end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+if board[index] == " " || board[index] == ""
+  return false
+end
+
+if board[index] = "nil"
+  return false
+end
+end
+def valid_move? (board, index)
+if index.between?(0, 8) || position_taken? (board, index)
+  return "false"
+end
+end
+#
+#
+# # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+# def position_taken?(board, index)
+#
+#    if board[index] == "X" || board[index] == "O"
+#     return true
+#   end
+#
+#   if board[index] == " " || board[index] == ""
+#     return false
+#   end
+#
+#   if board[index] = "nil"
+#     return false
+#   end
